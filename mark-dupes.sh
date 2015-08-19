81dee78807a076d773c1a5a9c305f3849b441426
Daniel Roberts <droberts@dimagi.com>
2015-08-19 15:23:15 -0400

    slit out helpers so they can be imported to both scripts

--- mark-dupes.sh
+++ mark-dupes.sh
@@ -1,39 +1,4 @@
-function psql_commcarehq() {
-  psql $PSQL_ARGS commcarehq
-}
-
-function remove-header-and-footer() {
-  sed 1,2d | sed '$d' | sed '$d'
-}
-
-function get-unique-duplicates() {
-  uniq -c | grep '^ *[^1 ]' | sed 's/^ *[0-9]* *//'
-}
-
-function rows-to-tuple-tuple() {
-  sed 's/^\(.*\)$/'"('\1')/" | sed 's/ *| */'"','/g" | python -c "import sys; print '(' + ','.join(sys.stdin.readlines()) + ')'"
-}
-
-function render-sql-template-from-rows() {
-  python -c 'import sys; print open("'"$1"'").read().format(rows=sys.stdin.read()),'
-}
-function mark-duplicates() {
-  while read line
-  do
-    CURRENT_LINE=$(echo $line | cut -d'|' -f2-)
-    if [[ $CURRENT_LINE = $PREV_LINE ]]
-    then
-      echo '✗' $line
-    else
-      echo '✓' $line
-    fi
-    PREV_LINE=$CURRENT_LINE
-  done
-}
-function pull-id-field-from-dupe-rows() {
-  grep '^✗' | cut -d' ' -f2
-}
-
+source helpers.sh
 psql_commcarehq < select-dupe-fields.sql | remove-header-and-footer |
   get-unique-duplicates |
   rows-to-tuple-tuple | render-sql-template-from-rows select-rows-with-ids.sql |
--- remove-dupes.sh
+++ remove-dupes.sh
@@ -1,4 +1,5 @@
+source helpers.sh
 bash mark-dupes.sh |
   pull-id-field-from-dupe-rows |
   rows-to-tuple-tuple | render-sql-template-from-rows delete-rows-by-id.sql |
-  psql $PSQL_ARGS commcarehq | remove-header-and-footer
+  psql_commcarehq | remove-header-and-footer
