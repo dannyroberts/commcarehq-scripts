cb8ae047380a3b79642f0544c5c94f4dceb9162e
Simon Kelly <skelly@dimagi.com>
2016-01-05 16:26:37 +0200

    Update github_open_commits.js

--- browser-scripts/github_open_commits.js
+++ browser-scripts/github_open_commits.js
@@ -1,15 +1,12 @@
 // ==UserScript==
 // @name         GitHub open commits in tabs
 // @namespace    github_open_commits
-// @version      0.1
+// @version      1
 // @description  Open all commits in a PR in new tabs. https://github.com/dimagi/commcarehq-scripts/blob/master/browser-scripts/FogBugz_Timesheet_Hours.user.js
 // @include      https://github.com/*/*/pull/*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @grant        GM_openInTab
 // ==/UserScript==
-/* jshint -W097 */
-'use strict';
-
 $(document).ready(function() {
     var hrefs = new Array();
     var elements = $('.commit-id');
